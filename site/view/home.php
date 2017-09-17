148b4809d784105cbe72636f5e6e2b7b73efcc7b
phamngan1129 <nganpham1129@gmail.com>
2017-09-17 13:35:23 +0700

    update slideShow

--- site/view/home.php
+++ site/view/home.php
@@ -6,51 +6,27 @@
         <div class="entry-slider">
           <div class="flexslider" id="flexslider-hero">
             <ul class="slides clearfix">
+            <?php
+              $sliders = new SlidersModel();
+              $showSlider = $sliders -> getAllSlider();
+              if (empty($showSlider)) {
+              } else {
+                foreach ($showSlider as $slide) {
+            ?>
               <li>
-                <img src="../../public/img/slider/1.jpg" alt="">
-                <div class="img-holder img-1"></div>
+                <img src="../../upload/sliders/<?php echo $slide['slide_image']; ?>" alt="aa" style="width: 100%; height: 536px;">
                 <div class="hero-holder text-center right-align">
                   <div class="hero-lines">
-                    <h1 class="hero-heading white">Collection 2017</h1>
-                    <h4 class="hero-subheading white uppercase">HOT AND FRESH TRENDS OF THIS YEAR</h4>
+                    <h1 class="hero-heading white"><?php echo $slide['slide_title']; ?></h1>
+                    <h4 class="hero-subheading white"><?php echo $slide['slide_description']; ?></h4>
                   </div>
-                  <a href="#" class="btn btn-lg btn-white"><span>Shop Now</span></a>
+                  <a href="<?php echo $slide['link']; ?>" class="btn btn-lg btn-white"><span>Xem chi tiết</span></a>
                 </div>
               </li>
-              <li>
-                <img src="../../public/img/slider/2.jpg" alt="">
-                <div class="img-holder img-2"></div>
-                <div class="hero-holder text-center">
-                  <div class="hero-lines">
-                    <h1 class="hero-heading white large">Winter Sales</h1>
-                  </div>
-                  <a href="#" class="btn btn-lg btn-white"><span>Shop Now</span></a>
-                </div>
-              </li>
-              <li>
-                <img src="../../public/img/slider/3.jpg" alt="">
-                <div class="img-holder img-3"></div>
-                <div class="hero-holder left-align">
-                  <div class="hero-lines">
-                    <h1 class="hero-heading white">Autumn 2017</h1>
-                    <p class="white">A-ha Theme is the Best E-Commerce solution</p>
-                    <p class="white">Packed with tons of features and unique styles</p>
-                  </div>
-                  <a href="#" class="btn btn-lg btn-white"><span>Shop Now</span></a>
-                </div>
-              </li>
-              <li>
-                <img src="../../public/img/slider/4.jpg" alt="">
-                <div class="img-holder img-4"></div>
-                <div class="hero-holder text-center right-align">
-                  <div class="hero-lines">
-                    <h1 class="hero-heading white">Summer 2017</h1>
-                    <p class="white">A-ha Theme is the Best E-Commerce solution</p>
-                    <p class="white">Packed with tons of features and unique styles</p>
-                  </div>
-                  <a href="#" class="btn btn-lg btn-white"><span>Shop Now</span></a>
-                </div>
-              </li>
+            <?php
+                }
+              }
+            ?>
             </ul>
           </div>
         </div> <!-- end slider -->
